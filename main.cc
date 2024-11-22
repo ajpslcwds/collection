bfeb524df4370f918e9ac001217e0eecb58c7f24
ajpslcwds <872929856@qq.com>
2024-11-22 16:22:54 +0800

    test_str_pp

--- main.cc
+++ main.cc
@@ -1,23 +1,45 @@
-#include <chrono>
+#include <atomic>
+#include <future>
 #include <iostream>
-#include <mutex>
-#include <sstream>
-#include <stdint.h>
+#include <memory>
+#include <stdio.h>
 #include <string.h>
+#include <string>
 #include <thread>
-
 using namespace std;
 
-int main()
+class Temp
 {
-    char temp[10] = "123456789";
-    cout << temp << endl;
-    strncpy(temp, "aaaaaaa", 3);
-    cout << temp << endl;
+  public:
+    std::atomic<int> a{1};
+    std::atomic<int> b = 1;
+    std::atomic<bool> bTemp = true;
+};
 
-    float f = 12345678.1234567;
-    printf("%.7f\n", f);
-        double d = 12345678.1234567;
-    printf("%.7f\n", d);
+void func()
+{
+    std::cout << "func_begin!" << std::endl;
+    std::async(std::launch::async, []() {
+        std::cout << "async_begin!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "async_end!" << std::endl;
+    });
+    std::cout << "func_end!" << std::endl;
+}
+int main(int argc, char *argv[])
+
+{
+    std::shared_ptr<Temp> temp = std::make_shared<Temp>();
+    std::shared_ptr<Temp> temp2 = nullptr;
+
+    temp2 = temp;
+    if (temp2 == temp)
+    {
+        printf("temp2 == temp\n");
+    }
+    else
+    {
+        printf("temp2 != temp\n");
+    }
     return 0;
 }
