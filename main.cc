a78f9601aa03fefb4024b281a478e32b7f0a3eab
ajpslcwds <872929856@qq.com>
2025-01-13 15:11:03 +0800

    type transfer

--- main.cc
+++ main.cc
@@ -1,23 +1,67 @@
-#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
-#include <mutex>
-#include <sstream>
-#include <stdint.h>
-#include <string.h>
-#include <thread>
+#include <string>
+#include <type_traits>
 
-using namespace std;
+template <typename T> struct always_false : std::false_type
+{
+};
+
+template <typename T> void WriteToBuffer(const std::string &valueStr, char *buffer, int &pos)
+{
+    T value;
+    if constexpr (std::is_same_v<T, int32_t>)
+    {
+        value = static_cast<int32_t>(std::atoi(valueStr.c_str()));
+    }
+    else if constexpr (std::is_same_v<T, uint32_t>)
+    {
+        value = static_cast<uint32_t>(std::stoul(valueStr));
+    }
+    else if constexpr (std::is_same_v<T, float>)
+    {
+        value = static_cast<float>(std::atof(valueStr.c_str()));
+    }
+    else if constexpr (std::is_same_v<T, std::string>)
+    {
+        value = valueStr;
+    }
+    else
+    {
+        static_assert(always_false<T>::value, "Unsupported type");
+    }
+
+    if constexpr (std::is_same_v<T, std::string>)
+    {
+        std::memcpy(buffer + pos, value.c_str(), value.size());
+        pos += value.size();
+    }
+    else
+    {
+        std::memcpy(buffer + pos, &value, sizeof(value));
+        pos += sizeof(value);
+    }
+}
 
 int main()
 {
-    char temp[10] = "123456789";
-    cout << temp << endl;
-    strncpy(temp, "aaaaaaa", 3);
-    cout << temp << endl;
+    char buffer[1024] = {0};
+    int pos = 0;
 
-    float f = 12345678.1234567;
-    printf("%.7f\n", f);
-        double d = 12345678.1234567;
-    printf("%.7f\n", d);
+    // Input values
+    WriteToBuffer<int32_t>("-123", buffer, pos);
+    WriteToBuffer<uint32_t>("221", buffer, pos);
+    WriteToBuffer<float>("12.12", buffer, pos);
+    WriteToBuffer<std::string>("abcde", buffer, pos);
+
+    // Print buffer content for verification (as hex)
+    for (int i = 0; i < pos; ++i)
+    {
+        printf("%02x ", static_cast<unsigned char>(buffer[i]));
+    }
+    printf("\n");
+
     return 0;
 }
