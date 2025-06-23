460352f8e46294d207f4319017b5634fae7729d1
ajpslcwds <872929856@qq.com>
2025-06-23 10:30:43 +0800

    floattoString

--- main.cc
+++ main.cc
@@ -13,26 +13,82 @@
 
 using namespace std;
 
+// int main()
+// {
+//     std::string str = "3.402823e+38";
+//     float f = std::stof(str);
+//     printf("%f\n", f);   // 340282306073709652508363335590014353408.000000
+//     printf("%e\n", f);   // 3.40282e+38
+//     printf("%g\n", f);   // 3.40282e+38
+//     printf("%.7g\n", f); // 3.402823e+38
+
+//     std::cout << "---------------------------------" << std::endl;
+//     float ff = 3.402823e+38;
+//     std::cout << std::scientific << ff << std::endl;
+//     std::cout << std::to_string(ff) << std::endl; // 340282306073709652508363335590014353408.000000
+
+//     return 0;
+// }
+
+std::string floatToAutoString(float value, int precision = 7)
+{
+    std::ostringstream oss;
+    oss << std::defaultfloat << std::setprecision(precision);
+    oss << value;
+    return oss.str();
+}
+
+std::string floatToAutoString(double value, int precision = 17)
+{
+    std::ostringstream oss;
+    oss << std::defaultfloat << std::setprecision(precision);
+    oss << value;
+    return oss.str();
+}
+
 int main()
 {
-    std::string str = "3.402823e+38";
-    float f = std::stof(str);
-    printf("%f\n", f);   // 340282306073709652508363335590014353408.000000
-    printf("%g\n", f);   // 3.40282e+38
-    printf("%.7g\n", f); // 3.402823e+38
+    {
+        double d = -1.7976931348623157e+308;
+        std::cout << d << std::endl;
+        std::cout << std::to_string(d) << std::endl;
+        printf("%f\n", d);
+        printf("%e\n", d);
+        printf("%g\n", d);
+        printf("%.7g\n", d);
+        std::cout << "---------------------------------" << std::endl;
+    }
 
-    float ff = 3.402823e+38;
-    std::cout << std::to_string(ff) << std::endl; // 340282306073709652508363335590014353408.000000
+    {
+        std::vector<float> vec = {0.0, 123.456, 0.000000123, 3.402823e+38, 1.175495e-38, -3.402823e+38, -1.175495e-38};
+        for (auto &f : vec)
+            std::cout << "f = " << floatToAutoString(f) << std::endl;
+        std::cout << "---------------------------------" << std::endl;
+    }
 
-    // double d = std::stod(str);
-    // long double ld = std::stold(str);
-    // std::cout << "float:      " << f << std::endl;   // 3.40282e+38
-    // std::cout << "double:      " << d << std::endl;  // 3.40282e+38
-    // std::cout << "long double: " << ld << std::endl; // 3.40282e+38
+    {
+        std::vector<double> vec = {0.0,
+                                   123.456,
+                                   0.000000123,
+                                   3.402823e+38,
+                                   1.175495e-38,
+                                   2.2250738585072014e-308,
+                                   1.7976931348623157e+308,
+                                   -2.2250738585072014e-308,
+                                   -1.7976931348623157e+308};
+        for (auto &d : vec)
+            std::cout << "d = " << floatToAutoString(d) << std::endl;
 
-    std::string strr;
-    strr.reserve(0);
-    cout << strr << endl;
+        std::cout << "---------------------------------" << std::endl;
+    }
+
+    {
+        double x = 1.0 / 0.0;
+        std::cout << "std::cout: " << x << std::endl;
+        std::string s = std::to_string(x);
+        std::cout << "std::to_string: " << s << std::endl;
+        std::cout << "---------------------------------" << std::endl;
+    }
 
     return 0;
 }
